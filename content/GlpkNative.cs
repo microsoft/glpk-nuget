fbf1bfbd12f5b3494be9744eb967cb42f6ef7a1f
Shon Katzenberger <shonk@microsoft.com>
2024-02-28 16:16:37 -0800

    Add test, content, pipelines, and package

---
+++ content/GlpkNative.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GlpkNativeApi;
+
+/// <summary>
+/// This is the raw GLPK native API. It can be extended as new entry points are determined to be needed.
+/// REVIEW: Perhaps include the full exported API now.
+/// </summary>
+internal static class GlpkNative
+{
+    private const string Library = "glpk";
+
+    public const int GLP_ON = 1; /* enable something */
+    public const int GLP_OFF = 0; /* disable something */
+
+    // optimization direction flag:
+    public const int GLP_MIN = 1; /* minimization */
+    public const int GLP_MAX = 2; /* maximization */
+
+    // kind of structural variable:
+    public const int GLP_CV = 1; /* continuous variable */
+    public const int GLP_IV = 2; /* integer variable */
+    public const int GLP_BV = 3; /* binary variable */
+
+    /* type of auxiliary/structural variable: */
+    public const int GLP_FR = 1; /* free (unbounded) variable */
+    public const int GLP_LO = 2; /* variable with lower bound */
+    public const int GLP_UP = 3; /* variable with upper bound */
+    public const int GLP_DB = 4; /* double-bounded variable */
+    public const int GLP_FX = 5; /* fixed variable */
+
+    /* solution status: */
+    public const int GLP_UNDEF = 1; /* solution is undefined */
+    public const int GLP_FEAS = 2; /* solution is feasible */
+    public const int GLP_INFEAS = 3; /* solution is infeasible */
+    public const int GLP_NOFEAS = 4; /* no feasible solution exists */
+    public const int GLP_OPT = 5; /* solution is optimal */
+    public const int GLP_UNBND = 6; /* solution is unbounded */
+
+    /* branching technique: */
+    public const int GLP_BR_FFV = 1; /* first fractional variable */
+    public const int GLP_BR_LFV = 2; /* last fractional variable */
+    public const int GLP_BR_MFV = 3; /* most fractional variable */
+    public const int GLP_BR_DTH = 4; /* heuristic by Driebeck and Tomlin */
+    public const int GLP_BR_PCH = 5; /* hybrid pseudocost heuristic */
+
+    /* backtracking technique: */
+    public const int GLP_BT_DFS = 1; /* depth first search */
+    public const int GLP_BT_BFS = 2; /* breadth first search */
+    public const int GLP_BT_BLB = 3; /* best local bound */
+    public const int GLP_BT_BPH = 4; /* best projection heuristic */
+
+    /* preprocessing technique: */
+    public const int GLP_PP_NONE = 0; /* disable preprocessing */
+    public const int GLP_PP_ROOT = 1; /* preprocessing only on root level */
+    public const int GLP_PP_ALL = 2; /* preprocessing on all levels */
+
+    /// <summary>
+    /// Mirrors the GLPK structure. Created by pasting from the source.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct glp_iocp
+    {
+        /* integer optimizer control parameters */
+        public int msg_lev;            /* message level (see glp_smcp) */
+        public int br_tech;            /* branching technique: */
+        public int bt_tech;            /* backtracking technique: */
+        public double tol_int;         /* mip.tol_int */
+        public double tol_obj;         /* mip.tol_obj */
+        public int tm_lim;             /* mip.tm_lim (milliseconds) */
+        public int out_frq;            /* mip.out_frq (milliseconds) */
+        public int out_dly;            /* mip.out_dly (milliseconds) */
+        public IntPtr cb_func; // void (* cb_func) (glp_tree* T, void* info); /* mip.cb_func */
+        public IntPtr cb_info; // void* cb_info;          /* mip.cb_info */
+        public int cb_size;            /* mip.cb_size */
+        public int pp_tech;            /* preprocessing technique: */
+        public double mip_gap;         /* relative MIP gap tolerance */
+        public int mir_cuts;           /* MIR cuts       (GLP_ON/GLP_OFF) */
+        public int gmi_cuts;           /* Gomory's cuts  (GLP_ON/GLP_OFF) */
+        public int cov_cuts;           /* cover cuts     (GLP_ON/GLP_OFF) */
+        public int clq_cuts;           /* clique cuts    (GLP_ON/GLP_OFF) */
+        public int presolve;           /* enable/disable using MIP presolver */
+        public int binarize;           /* try to binarize integer variables */
+        public int fp_heur;            /* feasibility pump heuristic */
+        public int ps_heur;            /* proximity search heuristic */
+        public int ps_tm_lim;          /* proxy time limit, milliseconds */
+        public int sr_heur;            /* simple rounding heuristic */
+        // #if 1 /* 24/X-2015; not documented--should not be used */
+        public int use_sol;            /* use existing solution */
+        public IntPtr save_sol; // const char *save_sol;   /* filename to save every new solution */
+        public int alien;              /* use alien solver */
+        // #endif
+        // #if 1 /* 16/III-2016; not documented--should not be used */
+        public int flip;               /* use long-step dual simplex */
+        // #endif
+        public fixed double foo_bar[23];     /* (reserved) */
+    }
+
+    /* message level: */
+    public const int GLP_MSG_OFF = 0; /* no output */
+    public const int GLP_MSG_ERR = 1; /* warning and error messages only */
+    public const int GLP_MSG_ON = 2; /* normal output */
+    public const int GLP_MSG_ALL = 3; /* full output */
+    public const int GLP_MSG_DBG = 4; /* debug output */
+
+    /* simplex method option: */
+    public const int GLP_PRIMAL = 1; /* use primal simplex */
+    public const int GLP_DUALP = 2; /* use dual; if it fails, use primal */
+    public const int GLP_DUAL = 3; /* use dual simplex */
+
+    /* pricing technique: */
+    public const int GLP_PT_STD = 0x11; /* standard (Dantzig's rule) */
+    public const int GLP_PT_PSE = 0x22; /* projected steepest edge */
+
+    /* ratio test technique: */
+    public const int GLP_RT_STD = 0x11; /* standard (textbook) */
+    public const int GLP_RT_HAR = 0x22; /* Harris' two-pass ratio test */
+    // #if true // /* 16/III-2016 */
+    public const int GLP_RT_FLIP = 0x33; /* long-step (flip-flop) ratio test */
+    // #endif
+
+    /* option to use A or N: */
+    public const int GLP_USE_AT = 1; /* use A matrix in row-wise format */
+    public const int GLP_USE_NT = 2; /* use N matrix in row-wise format */
+
+    /* return codes: */
+    public const int GLP_EBADB = 0x01; /* invalid basis */
+    public const int GLP_ESING = 0x02; /* singular matrix */
+    public const int GLP_ECOND = 0x03; /* ill-conditioned matrix */
+    public const int GLP_EBOUND = 0x04; /* invalid bounds */
+    public const int GLP_EFAIL = 0x05; /* solver failed */
+    public const int GLP_EOBJLL = 0x06; /* objective lower limit reached */
+    public const int GLP_EOBJUL = 0x07; /* objective upper limit reached */
+    public const int GLP_EITLIM = 0x08; /* iteration limit exceeded */
+    public const int GLP_ETMLIM = 0x09; /* time limit exceeded */
+    public const int GLP_ENOPFS = 0x0A; /* no primal feasible solution */
+    public const int GLP_ENODFS = 0x0B; /* no dual feasible solution */
+    public const int GLP_EROOT = 0x0C; /* root LP optimum not provided */
+    public const int GLP_ESTOP = 0x0D; /* search terminated by application */
+    public const int GLP_EMIPGAP = 0x0E; /* relative mip gap tolerance reached */
+    public const int GLP_ENOFEAS = 0x0F; /* no primal/dual feasible solution */
+    public const int GLP_ENOCVG = 0x10; /* no convergence */
+    public const int GLP_EINSTAB = 0x11; /* numerical instability */
+    public const int GLP_EDATA = 0x12; /* invalid data */
+    public const int GLP_ERANGE = 0x13; /* result out of range */
+
+    /// <summary>
+    /// Mirrors the GLPK structure. Created by pasting from the source.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct glp_smcp
+    {
+        /* simplex solver control parameters */
+        public int msg_lev;            /* message level: */
+        public int meth;               /* simplex method option: */
+        public int pricing;            /* pricing technique: */
+        public int r_test;             /* ratio test technique: */
+        public double tol_bnd;         /* primal feasibility tolerance */
+        public double tol_dj;          /* dual feasibility tolerance */
+        public double tol_piv;         /* pivot tolerance */
+        public double obj_ll;          /* lower objective limit */
+        public double obj_ul;          /* upper objective limit */
+        public int it_lim;             /* simplex iteration limit */
+        public int tm_lim;             /* time limit, ms */
+        public int out_frq;            /* display output frequency, ms */
+        public int out_dly;            /* display output delay, ms */
+        public int presolve;           /* enable/disable using LP presolver */
+        // #if 1 /* 11/VII-2017 (not documented yet) */
+        public int excl;               /* exclude fixed non-basic variables */
+        public int shift;              /* shift bounds of variables to zero */
+        public int aorn;               /* option to use A or N: */
+        public fixed double foo_bar[33];     /* (reserved) */
+        // #endif
+    }
+
+    // Problem api.
+    [DllImport(Library)] public static extern IntPtr glp_create_prob();
+    [DllImport(Library)] public static extern void glp_delete_prob(IntPtr prob);
+    [DllImport(Library)] public static extern int glp_get_num_rows(IntPtr prob);
+    [DllImport(Library)] public static extern int glp_get_num_cols(IntPtr prob);
+    [DllImport(Library)] public static extern int glp_add_rows(IntPtr prob, int nrs);
+    [DllImport(Library)] public static extern int glp_add_cols(IntPtr prob, int ncs);
+    [DllImport(Library)] public static extern void glp_set_row_bnds(IntPtr prob, int i, int type, double lb, double ub);
+    [DllImport(Library)] public static extern void glp_set_col_bnds(IntPtr prob, int j, int type, double lb, double ub);
+    [DllImport(Library)] public static extern void glp_set_col_kind(IntPtr prob, int j, int kind);
+    [DllImport(Library)] public static extern void glp_set_obj_dir(IntPtr prob, int dir);
+    [DllImport(Library)] public static extern void glp_set_obj_coef(IntPtr prob, int j, double coef);
+    [DllImport(Library)] public static extern void glp_set_mat_row(IntPtr prob, int i, int len, int[] ind, double[] val);
+
+    // Simplex api.
+    [DllImport(Library)] public static extern int glp_init_smcp(out glp_smcp parm);
+    [DllImport(Library)] public static extern int glp_simplex(IntPtr prob, ref glp_smcp parm);
+    [DllImport(Library)] public static extern int glp_get_status(IntPtr prob);
+    [DllImport(Library)] public static extern double glp_get_obj_val(IntPtr prob);
+    [DllImport(Library)] public static extern double glp_get_col_prim(IntPtr prob, int j);
+
+    // MIP api.
+    [DllImport(Library)] public static extern void glp_init_iocp(out glp_iocp parm);
+    [DllImport(Library)] public static extern int glp_intopt(IntPtr prob, ref glp_iocp parm);
+    [DllImport(Library)] public static extern int glp_mip_status(IntPtr prob);
+    [DllImport(Library)] public static extern double glp_mip_obj_val(IntPtr prob);
+    [DllImport(Library)] public static extern double glp_mip_col_val(IntPtr prob, int j);
+}
---
+++ test/BasicTests.cs
@@ -0,0 +1,143 @@
+﻿
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GlpkTest;
+
+using Native = GlpkNativeApi.GlpkNative;
+
+[TestClass]
+public sealed class BasicTests
+{
+    [TestMethod]
+    public void CreateProblem()
+    {
+        var prob = Native.glp_create_prob();
+        try
+        {
+        }
+        finally
+        {
+            Native.glp_delete_prob(prob);
+        }
+    }
+
+    [TestMethod]
+    public void AddRowsAndCols()
+    {
+        var prob = Native.glp_create_prob();
+        try
+        {
+            int r = Native.glp_add_rows(prob, 3);
+            int c = Native.glp_add_cols(prob, 5);
+            Assert.AreEqual(1, r);
+            Assert.AreEqual(1, c);
+            Assert.AreEqual(3, Native.glp_get_num_rows(prob));
+            Assert.AreEqual(5, Native.glp_get_num_cols(prob));
+        }
+        finally
+        {
+            Native.glp_delete_prob(prob);
+        }
+    }
+
+    [TestMethod]
+    public void Simplex()
+    {
+        var prob = Native.glp_create_prob();
+        try
+        {
+            int r = Native.glp_add_rows(prob, 1);
+            int c = Native.glp_add_cols(prob, 2);
+            Assert.AreEqual(1, r);
+            Assert.AreEqual(1, c);
+            Assert.AreEqual(1, Native.glp_get_num_rows(prob));
+            Assert.AreEqual(2, Native.glp_get_num_cols(prob));
+
+            // Note that indices are 1-based!
+
+            // Define the objective: maximize x + y.
+            Native.glp_set_obj_coef(prob, 1, 10);
+            Native.glp_set_obj_coef(prob, 2, 11);
+            Native.glp_set_obj_dir(prob, Native.GLP_MAX);
+
+            // Set variable bounds.
+            Native.glp_set_col_bnds(prob, 1, Native.GLP_DB, 0, 5.5);
+            Native.glp_set_col_bnds(prob, 2, Native.GLP_DB, 0, 5.5);
+
+            // x + y <= 8.
+            Native.glp_set_row_bnds(prob, 1, Native.GLP_UP, double.NegativeInfinity, 8);
+            // Note that because indices are 1-based, there needs to be a dummy initial term in each array.
+            // These values are ignored by GLPK, but needed!
+            Native.glp_set_mat_row(prob, 1, 2, new[] { -1, 1, 2 }, new[] { double.NaN, 1.0, 1.0 });
+
+            // Solve.
+            Native.glp_init_smcp(out var smcp);
+            smcp.presolve = Native.GLP_ON;
+            var res = Native.glp_simplex(prob, ref smcp);
+            int status = Native.glp_get_status(prob);
+
+            // Verify.
+            Assert.AreEqual(0, res);
+            Assert.AreEqual(Native.GLP_OPT, status);
+            Assert.AreEqual(2.5, Native.glp_get_col_prim(prob, 1));
+            Assert.AreEqual(5.5, Native.glp_get_col_prim(prob, 2));
+            Assert.AreEqual(85.5, Native.glp_get_obj_val(prob));
+        }
+        finally
+        {
+            Native.glp_delete_prob(prob);
+        }
+    }
+
+    [TestMethod]
+    public void Mip()
+    {
+        var prob = Native.glp_create_prob();
+        try
+        {
+            int r = Native.glp_add_rows(prob, 1);
+            int c = Native.glp_add_cols(prob, 2);
+            Assert.AreEqual(1, r);
+            Assert.AreEqual(1, c);
+            Assert.AreEqual(1, Native.glp_get_num_rows(prob));
+            Assert.AreEqual(2, Native.glp_get_num_cols(prob));
+
+            // Note that indices are 1-based!
+
+            // Define the objective: maximize x + y.
+            Native.glp_set_obj_coef(prob, 1, 10);
+            Native.glp_set_obj_coef(prob, 2, 11);
+            Native.glp_set_obj_dir(prob, Native.GLP_MAX);
+
+            // Set variable bounds and kinds.
+            Native.glp_set_col_kind(prob, 1, Native.GLP_IV);
+            Native.glp_set_col_bnds(prob, 1, Native.GLP_DB, 0, 5);
+            Native.glp_set_col_bnds(prob, 2, Native.GLP_DB, 0, 5.5);
+
+            // x + y <= 8.
+            Native.glp_set_row_bnds(prob, 1, Native.GLP_UP, double.NegativeInfinity, 8);
+            // Note that because indices are 1-based, there needs to be a dummy initial term in each array.
+            // These values are ignored by GLPK, but needed!
+            Native.glp_set_mat_row(prob, 1, 2, new[] { -1, 1, 2 }, new[] { double.NaN, 1.0, 1.0 });
+
+            // Solve.
+            Native.glp_init_iocp(out var iocp);
+            iocp.presolve = Native.GLP_ON;
+            var res = Native.glp_intopt(prob, ref iocp);
+            int status = Native.glp_mip_status(prob);
+
+            // Verify.
+            Assert.AreEqual(0, res);
+            Assert.AreEqual(Native.GLP_OPT, status);
+            Assert.AreEqual(3, Native.glp_mip_col_val(prob, 1));
+            Assert.AreEqual(5, Native.glp_mip_col_val(prob, 2));
+            Assert.AreEqual(85, Native.glp_mip_obj_val(prob));
+        }
+        finally
+        {
+            Native.glp_delete_prob(prob);
+        }
+    }
+}
